fc1a263c9a5a57c46db82eee786ac1af33e068b8
wasifsomji <wasifsomji@gmail.com>
2024-03-01 12:53:48 -0500

    removed button

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -37,7 +37,7 @@ const Home = () => {
                 <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={15} /> 
                 </h1>
                 <h2>Computer Engineering 25' at McGill University</h2>
-                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                {/* <Link to="/contact" className="flat-button">CONTACT ME</Link> */}
             </div>
             <Logo className="Home"/>
         </div>
