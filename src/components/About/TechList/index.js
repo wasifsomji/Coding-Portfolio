f5a37e531c1d3255fec4d6394f4a08303be3c8e5
wasifsomji <wasifsomji@gmail.com>
2024-04-25 01:10:20 -0400

    updated order

--- src/components/About/TechList/index.js
+++ src/components/About/TechList/index.js
@@ -6,9 +6,12 @@ const TechList = () => {
   const [techs] = useState([
     { name: "NodeJS", progress: 100, className: "node-progress", years: 1},
     { name: "ReactJS", progress: 100, className: "react-progress", years: 1 },
-    { name: "VueJS", progress: 100, className: "react-progress", years: 1 },
+    { name: "VueJS", progress: 100, className: "vue-progress", years: 1 },
     { name: "Spring Boot", progress: 100, className: "springboot-progress", years: 1 },
     { name: "Express.js", progress: 100, className: "junit-progress", years: 1 },
+    { name: "PostgreSQL", progress: 100, className: "postgresql-progress", years: 1 },
+    { name: "MongoDB", progress: 100, className: "mongodb-progress", years: 1 }
+
 
   ]);
 
