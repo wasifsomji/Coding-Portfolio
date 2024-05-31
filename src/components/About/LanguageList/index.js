513826e154b5ffe3e69132beca5d0d71306bb331
wasifsomji <wasifsomji@gmail.com>
2024-05-31 18:34:09 -0400

    updated portfolio skills

--- src/components/About/LanguageList/index.js
+++ src/components/About/LanguageList/index.js
@@ -5,7 +5,8 @@ import "./index.scss"
 const LanguageList = () => {
   const [languages] = useState([
     { name: "Java", progress: 100, className: "java-progress", years: 3 },
-    { name: "Python", progress: 66, className: "python-progress", years: 2 },
+    { name: "Python", progress: 100, className: "python-progress", years: 2 },
+    { name: "Go", progress: 20, className: "go-progress", years: 1 },
     { name: "HTML/CSS/JS", progress: 66, className: "web-progress", years: 2 },
     { name: "C", progress: 33, className: "c-progress", years: 1 },
     { name: "ARM7 Assembly", progress: 33, className: "arm7-assembly-progress", years: 1 },
